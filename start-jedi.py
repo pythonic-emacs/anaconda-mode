a44ba4e219325bc7c6be5c0424e3fefe0cfca846
Malyshev Artem <proofit404@gmail.com>
2013-11-19 15:52:04 +0400

    Add logging to jedi server.

--- start-jedi.py
+++ start-jedi.py
@@ -2,6 +2,11 @@
 
 import http.server as server
 import json
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 def do_jedi(req):
@@ -14,10 +19,19 @@ class JediHandler(server.BaseHTTPRequestHandler):
     def do_POST(self):
         """Process client POST request."""
 
+        logger.debug('Processing request...')
+
         content_len = int(self.headers['content-length'])
+        logger.debug('Content length: %s', content_len)
+
         request = self.rfile.read(content_len).decode()
+        logger.debug('Accepted content: %s', request)
+
         result = do_jedi(json.loads(request))
+        logger.debug('Jedi result: %s', result)
+
         message = json.dumps(result).encode()
+        logger.debug('Send message to host: %s', message)
 
         self.send_response(200)
         self.end_headers()
