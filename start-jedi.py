181cad14b941da47b375b3561e2f1242ddb5c685
Malyshev Artem <proofit404@gmail.com>
2013-11-19 14:31:51 +0400

    Add ugly json support on POST requests.

--- start-jedi.py
+++ start-jedi.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-from http import server
+import http.server as server
+import json
+
+
+def do_jedi(req):
+    """Process Jedi operation correspond to request."""
+    return req
 
 
 class JediHandler(server.BaseHTTPRequestHandler):
@@ -8,9 +14,15 @@ class JediHandler(server.BaseHTTPRequestHandler):
     def do_POST(self):
         """Process client POST request."""
 
+        content_len = int(self.headers['content-length'])
+        request = self.rfile.read(content_len).decode()
+        result = do_jedi(json.loads(request))
+        message = json.dumps(result).encode()
+
         self.send_response(200)
         self.end_headers()
-        self.wfile.write('Hello\r\n'.encode())
+        self.wfile.write(message)
+
         return
 
 
