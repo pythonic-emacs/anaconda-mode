766e9bda1e15fbb4dfc38881081af726cff86adb
Malyshev Artem <proofit404@gmail.com>
2013-11-21 15:55:16 +0400

    Add testing log formatter similar to default.

--- test/helper.py
+++ test/helper.py
@@ -6,5 +6,8 @@ logger = logging.getLogger('start_jedi')
 handler = logging.FileHandler('log/tests.log')
 handler.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+handler.setFormatter(formatter)
+
 logger.addHandler(handler)
 logger.propagate = False  # Not echo to console
