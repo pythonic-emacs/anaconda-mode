fe158007343b5fc83e37cc9789c09542ba48a71b
Malyshev Artem <proofit404@gmail.com>
2013-11-21 21:06:49 +0400

    Add elpa header to emacs lisp package part.

---
+++ company-jedi.el
@@ -0,0 +1,28 @@
+;;; company-jedi.el --- Jedi backend for company-mode
+
+;; Copyright (C) 2013 by Malyshev Artem
+
+;; Authors: Malyshev Artem <proofit404@gmail.com>
+;; URL: https://github.com/proofit404/company-jedi
+;; Package-Requires: ((company "0.6.12"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'company-jedi)
+
+;;; company-jedi ends here
