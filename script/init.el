b2f6c5cfdc6c30d6c8f0bffd3258b52f88ac2d15
Malyshev Artem <proofit404@gmail.com>
2013-11-26 13:18:48 +0400

    Use custom start_jedi port on make emacs target.
    For avoid conflict with installed company-jedi package.

--- script/init.el
+++ script/init.el
@@ -1,6 +1,12 @@
 (require 'cl)
 (require 'company)
 
+(setq company-jedi-port 8000)
+
+(setq company-jedi-command
+      (concat (file-name-as-directory user-emacs-directory)
+              "jedi/venv/bin/python3 -m start_jedi --debug"))
+
 (add-to-list 'company-backends 'company-jedi)
 (add-hook 'python-mode-hook 'company-jedi-start)
 
