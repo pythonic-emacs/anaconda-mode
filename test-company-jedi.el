d7c5bb17479d9191a9c69971eea8e27859723376
Malyshev Artem <proofit404@gmail.com>
2014-01-25 21:23:49 +0400

    Use test fixtures in ert tests.
    Remove hardcoded python sources in tests definitions.

--- test-company-jedi.el
+++ test-company-jedi.el
@@ -9,16 +9,16 @@
 
 (ert-deftest test-jedi-candidates-json ()
   "Test jedi completion json request string generator."
-  (make-test-file "import json; json.l" 1 19)
+  (load-fixture "test/jedi_candidates/fixtures/simple.py" 2 11)
   (should (equal (company-jedi-request-json "candidates")
                  (concat "{"
                          ""    "\"command\":\"candidates\", "
                          ""    "\"attributes\":{"
-                         ""          "\"source\":\"import json; json.l\", "
-                         ""          "\"line\":1, "
-                         ""          "\"column\":19, "
-                         ""          "\"point\":19, "
-                         ""          "\"path\":" (company-jedi-encode default-path) ", "
+                         ""          "\"source\":" (company-jedi-encode (buffer-string)) ", "
+                         ""          "\"line\":2, "
+                         ""          "\"column\":11, "
+                         ""          "\"point\":27, "
+                         ""          "\"path\":" (company-jedi-encode (buffer-file-name)) ", "
                          ""          "\"company_prefix\":\"\", "
                          ""          "\"company_arg\":\"\""
                          ""    "}"
@@ -26,22 +26,22 @@
 
 (ert-deftest test-jedi-candidates ()
   "Completion request must return candidates list."
-  (make-test-file "import json; json.l" 1 19)
+  (load-fixture "test/jedi_candidates/fixtures/simple.py" 2 11)
   (should (equal (company-jedi-candidates)
-                 '("load" "loads"))))
+                 '("date" "datetime" "datetime_CAPI"))))
 
 (ert-deftest test-jedi-location-json ()
   "Test doto_definition json generator."
-  (make-test-file)
+  (load-fixture "test/jedi_location/fixtures/simple.py" 8 1)
   (should (equal (company-jedi-request-json "location")
                  (concat "{"
                          ""    "\"command\":\"location\", "
                          ""    "\"attributes\":{"
-                         ""          "\"source\":" (company-jedi-encode default-content) ", "
+                         ""          "\"source\":" (company-jedi-encode (buffer-string)) ", "
                          ""          "\"line\":8, "
                          ""          "\"column\":1, "
                          ""          "\"point\":103, "
-                         ""          "\"path\":" (company-jedi-encode default-path) ", "
+                         ""          "\"path\":" (company-jedi-encode (buffer-file-name)) ", "
                          ""          "\"company_prefix\":\"\", "
                          ""          "\"company_arg\":\"\""
                          ""    "}"
@@ -49,36 +49,28 @@
 
 (ert-deftest test-jedi-location ()
   "Test find definition at point."
-  (make-test-file)
-  (should (equal (company-jedi-location)
-                 (cons default-path 1)))) ; Fist definition will chose by mocked user.
+  (let ((fixture "test/jedi_location/fixtures/simple.py"))
+    (load-fixture fixture 8 1)
+    (should (equal (company-jedi-location)
+                   (cons (fixture-path fixture) 1))))) ; Fist definition will chose by mocked user.
 
 (ert-deftest test-jedi-multiple-location ()
   "Test non determined locations."
-  (make-test-file "if True:
-    a = 1
-else:
-    a = \"a\"
-
-a" 6 1)
-  (should (equal (company-jedi-location)
-                 (cons default-path 2))))
+  (let ((fixture "test/jedi_location/fixtures/builtins.py"))
+    (load-fixture fixture 6 1)
+    (should (equal (company-jedi-location)
+                   (cons (fixture-path fixture) 2)))))
 
 (ert-deftest test-jedi-reference ()
   "Test fund references."
-  (make-test-file "def a():
-    pass
-
-v = a(1)
-m = a('t')" 1 4)
-  (should (equal (company-jedi-reference)
-                 (cons default-path 4))))
+  (let ((fixture "test/jedi_reference/fixtures/simple.py"))
+    (load-fixture fixture 1 4)
+    (should (equal (company-jedi-reference)
+                   (cons (fixture-path fixture) 4)))))
 
 (ert-deftest test-jedi-doc ()
   "Test found documentation string."
-  (make-test-file "def f(a, b=1):
-    \"Document for function f.\"
-    pass" 1 4)
+  (load-fixture "test/jedi_doc/fixtures/simple.py" 1 4)
   (should (equal "Document for function f."
                  (with-current-buffer (company-jedi-doc-buffer)
                    (buffer-string)))))
@@ -93,37 +85,20 @@ m = a('t')" 1 4)
 
 (ert-deftest test-jedi-meta ()
   "Company must ignore empty doc strings."
-  (make-test-file "def f(a, b=1):
-    pass" 1 4)
+  (load-fixture "test/jedi_doc/fixtures/docless.py" 1 4)
   (should (null (company-jedi-meta))))
 
 ;; Helper functions.
 
-(defvar root-directory (file-name-directory load-file-name))
+(defun fixture-path (file)
+  "Make absolute path from project path."
+  (concat (file-name-directory load-file-name) file))
 
-(defvar default-content
-  "def my_func():
-    print 'called'
-
-alias = my_func
-my_list = [1, None, alias]
-inception = my_list[2]
-
-inception()")
-
-(defvar default-line 8)
-
-(defvar default-column 1)
-
-(defvar default-path (concat root-directory "log/simple.py"))
-
-(defun make-test-file (&optional content line column path)
-  "Create test file."
-  (find-file (or path default-path))
-  (erase-buffer)  ;; Remove side effect from multiple open-file operations.
-  (insert (or content default-content))
-  (goto-line (or line default-line))
-  (move-to-column (or column default-column)))
+(defun load-fixture (file line column)
+  "Open fixture file."
+  (find-file-read-only (fixture-path file))
+  (goto-line line)
+  (move-to-column column))
 
 (defun mock-completing-read (prompt collection)
   "Emulate user chose."
