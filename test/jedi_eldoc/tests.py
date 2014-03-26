ffcb262849ceb21d0b6e1c6a37815f08e314740f
Malyshev Artem <proofit404@gmail.com>
2014-03-26 22:40:51 +0400

    Fix naming issue in eldoc api test.

--- test/jedi_eldoc/tests.py
+++ test/jedi_eldoc/tests.py
@@ -1,6 +1,4 @@
-from test import TestCase
-from anaconda_mode import anaconda
-from test import editor
+from test import TestCase, editor
 
 
 class EldocTest(TestCase):
@@ -8,18 +6,18 @@ class EldocTest(TestCase):
     def test_eldoc(self):
         """Should return signature string."""
 
-        request = editor('test/jedi_eldoc/fixtures/simple.py', 8, 5, 'eldoc')
+        response = editor('test/jedi_eldoc/fixtures/simple.py', 8, 5, 'eldoc')
 
-        response = ("dump(obj, fp, skipkeys = False, ensure_ascii = True, "
+        expected = ("dump(obj, fp, skipkeys = False, ensure_ascii = True, "
                     "check_circular = True, allow_nan = True, cls = None, "
                     "indent = None, separators = None, default = None, "
                     "sort_keys = False, **kw)")
 
-        self.assertEqual(response, anaconda.process(**request))
+        self.assertEqual(response, expected)
 
     def test_empty_eldoc(self):
         """Don't answer eldoc on unknown functions."""
 
-        request = editor('test/jedi_eldoc/fixtures/broken.py', 4, 6, 'eldoc')
+        response = editor('test/jedi_eldoc/fixtures/broken.py', 4, 6, 'eldoc')
 
-        self.assertIsNone(anaconda.process(**request))
+        self.assertIsNone(response)
