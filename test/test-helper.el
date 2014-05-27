55cdb4ed1f1accc5a4a1e9f1d8c46b7366256912
Malyshev Artem <proofit404@gmail.com>
2014-05-27 11:20:17 +0400

    Organize test-helper.el

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,32 +1,12 @@
-;;; test-helper --- ert-runner test helper
-
-;;; Commentary:
-
-;;; Code:
-
 (require 'cask)
 
 (let ((source-directory (locate-dominating-file load-file-name "Cask")))
   (cask-initialize source-directory)
   (add-to-list 'load-path source-directory))
 
-;; Helper functions.
+;; Package setup.
 
-(defun fixture-path (file)
-  "Make absolute path from project path."
-  (concat anaconda-mode-directory file))
-
-(defun load-fixture (file line column)
-  "Open fixture file."
-  (find-file-read-only (fixture-path file))
-  (goto-line line)
-  (move-to-column column))
-
-(defun mock-completing-read (prompt collection &rest ignored)
-  "Emulate user chose."
-  (car (sort collection 'string<)))
-
-(setq completing-read-function 'mock-completing-read)
+(require 'anaconda-mode)
 
 (let ((envdir (getenv "ENVDIR")))
   (setq python-shell-virtualenv-path envdir))
@@ -35,10 +15,21 @@
 
 (setq anaconda-mode-debug t)
 
-;; Server bootstrap.
+;; Helper functions.
 
-(require 'anaconda-mode)
+(defun fixture-path (file)
+  "Make absolute FILE path from project path."
+  (concat anaconda-mode-directory file))
 
-(anaconda-mode-start-node)
+(defun load-fixture (file line column)
+  "Open fixture FILE at LINE COLUMN."
+  (find-file-read-only (fixture-path file))
+  (goto-char (point-min))
+  (forward-line (1- line))
+  (move-to-column column))
 
-(sleep-for 5) ;; Wait for anaconda_mode server will ready to work.
+(defun mock-completing-read (prompt collection &rest ignored)
+  "Emulate user chose for PROMPT COLLECTION skipping IGNORED."
+  (car (sort collection 'string<)))
+
+(setq completing-read-function 'mock-completing-read)
