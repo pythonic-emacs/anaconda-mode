98e405ab65ba512af4c9bb8775b08e01a63f4567
Fredrik Bergroth <fbergroth@gmail.com>
2014-06-01 12:00:54 +0200

    Further integration with company-mode
    - show type annotation (module/class/instance/function)
    - C-w to view candidate definition during completion
    - maintains ordering from jedi (now __methods__ are no longer on top)

--- company-anaconda.el
+++ company-anaconda.el
@@ -2,7 +2,8 @@
 
 ;; Copyright (C) 2013, 2014 by Malyshev Artem
 
-;; Author: Malyshev Artem <proofit404@gmail.com>
+;; Authors: Malyshev Artem <proofit404@gmail.com>
+;;          Fredrik Bergroth <fbergroth@gmail.com>
 ;; URL: https://github.com/proofit404/anaconda-mode
 ;; Version: 0.1.0
 
@@ -25,7 +26,15 @@
 
 (require 'company)
 (require 'anaconda-mode)
+(require 'dash)
 (eval-when-compile (require 'cl))
+
+(defvar company-anaconda-compact-annotation t
+  "Show only the first character of type in annotations.")
+
+(defun company-anaconda-init ()
+  "Initialize company-anaconda buffer."
+  (setq-local company-tooltip-align-annotations t))
 
 (defun company-anaconda-prefix ()
   "Grab prefix at point.
@@ -39,9 +48,13 @@ Properly detect strings, comments and attribute access."
 (defun company-anaconda-candidates ()
   "Obtain candidates list from anaconda."
   (mapcar (lambda (h)
-            (let ((candidate (gethash "name" h)))
-              (put-text-property 0 1 'short-doc (gethash "short_doc" h) candidate)
-              (put-text-property 0 1 'doc (gethash "doc" h) candidate)
+            (let ((candidate (plist-get h :name)))
+              (put-text-property 0 1 'doc (plist-get h :doc) candidate)
+              (put-text-property 0 1 'short-doc (plist-get h :short_doc) candidate)
+              (put-text-property 0 1 'annotation (plist-get h :annotation) candidate)
+              (-when-let* ((path (plist-get h :path))
+                           (line (plist-get h :line)))
+                (put-text-property 0 1 'location (cons path line) candidate))
               candidate))
           (anaconda-mode-complete)))
 
@@ -54,17 +67,32 @@ Properly detect strings, comments and attribute access."
   "Return short documentation string for chosen CANDIDATE."
   (get-text-property 0 'short-doc candidate))
 
+(defun company-anaconda-annotation (candidate)
+  "Return annotation string for chosen CANDIDATE."
+  (let ((annotation (get-text-property 0 'annotation candidate)))
+    (if company-anaconda-compact-annotation
+        (substring annotation 0 1)
+      annotation)))
+
+(defun company-anaconda-location (candidate)
+  "Return location (path . line) for chosen CANDIDATE."
+  (get-text-property 0 'location candidate))
+
 ;;;###autoload
 (defun company-anaconda (command &optional arg)
   "Jedi backend for company-mode.
 See `company-backends' for more info about COMMAND and ARG."
   (interactive (list 'interactive))
   (case command
+    (init (company-anaconda-init))
     (interactive (company-begin-backend 'company-anaconda))
     (prefix (company-anaconda-prefix))
     (candidates (company-anaconda-candidates))
     (doc-buffer (company-anaconda-doc-buffer arg))
-    (meta (company-anaconda-meta arg))))
+    (meta (company-anaconda-meta arg))
+    (annotation (company-anaconda-annotation arg))
+    (location (company-anaconda-location arg))
+    (sorted t)))
 
 (provide 'company-anaconda)
 
