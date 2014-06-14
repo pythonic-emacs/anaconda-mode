5d28cc7bcb80bd018afee9614d7e4a6db5828142
Fredrik Bergroth <fbergroth@gmail.com>
2014-06-14 21:58:22 +0200

    Set up plugin structure

--- anaconda-mode.el
+++ anaconda-mode.el
@@ -28,7 +28,6 @@
 (require 'json-rpc)
 (require 'etags)
 (require 'python)
-(require 'anaconda-nav)
 
 
 ;;; Server.
@@ -42,24 +41,7 @@
 (defvar anaconda-mode-port 24970
   "Port for anaconda_mode connection.")
 
-(defun anaconda-mode-python ()
-  "Detect python executable."
-  (let ((virtualenv python-shell-virtualenv-path))
-    (if virtualenv
-        (concat (file-name-as-directory virtualenv) "bin/python")
-      "python")))
-
-(defun anaconda-mode-python-args ()
-  "Python arguments to run anaconda_mode server."
-  (delq nil (list "anaconda_mode.py"
-                  "--bind" anaconda-mode-host
-                  "--port" (number-to-string anaconda-mode-port)
-                  (when anaconda-mode-debug "--debug"))))
-
-(defun anaconda-mode-command ()
-  "Shell command to run anaconda_mode server."
-  (cons (anaconda-mode-python)
-	(anaconda-mode-python-args)))
+(defvar anaconda-mode-plugins '(company nav doc eldoc))
 
 (defvar anaconda-mode-directory
   (file-name-directory load-file-name)
@@ -70,6 +52,38 @@
 
 (defvar anaconda-mode-connection nil
   "Json Rpc connection to anaconda_mode process.")
+
+(defun anaconda-mode-python ()
+  "Detect python executable."
+  ;; TODO: (f-join it "bin" "python")?
+  (--if-let python-shell-virtualenv-path
+      (concat (file-name-as-directory it) "bin/python")
+    "python"))
+
+(defun anaconda-mode-plugin-paths ()
+  "List of paths for anaconda plugins."
+  (->> anaconda-mode-plugins
+    (--map (->> it
+             symbol-name
+             (concat "anaconda-")
+             locate-library
+             file-name-directory))
+    -distinct))
+
+(defun anaconda-mode-python-args ()
+  "Python arguments to run anaconda_mode server."
+  (-flatten
+   (list "anaconda_mode.py"
+         "--bind" anaconda-mode-host
+         "--port" (number-to-string anaconda-mode-port)
+         (--mapcat `("--path" ,it) (anaconda-mode-plugin-paths))
+         (--mapcat `("--plugin" ,(symbol-name it)) anaconda-mode-plugins)
+         (when anaconda-mode-debug "--debug"))))
+
+(defun anaconda-mode-command ()
+  "Shell command to run anaconda_mode server."
+  (cons (anaconda-mode-python)
+        (anaconda-mode-python-args)))
 
 (defun anaconda-mode-running-p ()
   "Check for running anaconda_mode server."
@@ -88,7 +102,8 @@
                  (anaconda-mode-python-args)))
     (accept-process-output anaconda-mode-process)
     (setq anaconda-mode-connection
-          (json-rpc-connect anaconda-mode-host anaconda-mode-port))))
+          (json-rpc-connect anaconda-mode-host anaconda-mode-port))
+    (anaconda-mode-notify-plugins 'start)))
 
 (defun anaconda-mode-start-node ()
   "Start anaconda_mode server."
@@ -102,7 +117,8 @@
   (when (anaconda-mode-running-p)
     (kill-process anaconda-mode-process)
     (json-rpc-close anaconda-mode-connection)
-    (setq anaconda-mode-connection nil)))
+    (setq anaconda-mode-connection nil)
+    (anaconda-mode-notify-plugins 'stop)))
 
 (defun anaconda-mode-need-restart ()
   "Check if current `anaconda-mode-process' need restart with new args.
@@ -114,19 +130,17 @@ Return nil if it run under proper environment."
 
 ;;; Interaction.
 
-(defun anaconda-mode-call (command &rest args)
-  "Make remote procedure call for COMMAND.
-ARGS are COMMAND argument passed to remote call."
+(defun anaconda-rpc (method &rest params)
+  "Call rpc method METHOD with params PARAMS."
   (anaconda-mode-start-node)
   ;; use list since not all dash functions operate on vectors
   (let ((json-array-type 'list))
-    (apply 'json-rpc anaconda-mode-connection command args)))
+    (apply 'json-rpc anaconda-mode-connection method params)))
 
-(defun anaconda-mode-call-1 (command)
-  ;; TODO: Remove this function ones plugin system will be implemented.
-  ;; See #28.
-  (anaconda-mode-call
-   command
+(defun anaconda-rpc-script (method)
+  "Call rpc script method METHOD."
+  (anaconda-rpc
+   method
    (buffer-substring-no-properties (point-min) (point-max))
    (line-number-at-pos (point))
    (current-column)
@@ -135,13 +149,7 @@ ARGS are COMMAND argument passed to remote call."
 
 ;;; Minor mode.
 
-(defvar anaconda-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "M-?") 'anaconda-mode-view-doc)
-    (define-key map (kbd "M-r") 'anaconda-mode-usages)
-    (define-key map [remap find-tag] 'anaconda-mode-goto)
-    (define-key map [remap pop-tag-mark] 'anaconda-nav-pop-marker)
-    map)
+(defvar anaconda-mode-map (make-sparse-keymap)
   "Keymap for `anaconda-mode'.")
 
 ;;;###autoload
@@ -151,11 +159,18 @@ ARGS are COMMAND argument passed to remote call."
 \\{anaconda-mode-map}"
   :lighter " Anaconda"
   :keymap anaconda-mode-map
-  (if anaconda-mode
-      (add-hook 'completion-at-point-functions
-                'anaconda-mode-complete-at-point nil t)
-    (remove-hook 'completion-at-point-functions
-                 'anaconda-mode-complete-at-point t)))
+  (anaconda-mode-start-node)
+  (anaconda-mode-notify-plugins
+   (if anaconda-mode 'buffer-start 'buffer-stop)))
+
+(defun anaconda-mode-notify-plugins (step)
+  "Notify plugins of STEP through their handler."
+  (--each anaconda-mode-plugins
+    (let* ((library (concat "anaconda-" (symbol-name it)))
+           (handler (intern (concat library "-handler"))))
+      (require (intern library))
+      (when (fboundp handler)
+        (funcall handler step)))))
 
 (provide 'anaconda-mode)
 
--- anaconda_mode.py
+++ anaconda_mode.py
@@ -2,29 +2,34 @@ import logging
 import os
 import sys
 
+
+def add_package_path(base):
+    if base not in sys.path:
+        sys.path.append(base)
+
+    # Add every directory inside vendor/ to sys.path.
+    vendor = os.path.join(base, 'vendor')
+    for file in os.listdir(vendor):
+        path = os.path.join(vendor, file)
+        if os.path.isdir(path) and path not in sys.path:
+            sys.path.append(path)
+
+BASE_DIR = os.path.dirname(__file__)
+add_package_path(BASE_DIR)
+
+logger = logging.getLogger(__name__)
+
 PY3 = sys.version_info[0] == 3
 
 if PY3:
     from http.server import BaseHTTPRequestHandler, HTTPServer
+    from importlib import import_module
 else:
     from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-BASE_DIR = os.path.dirname(__file__)
-LOG_DIR = os.path.join(BASE_DIR, 'log')
-logger = logging.getLogger(__name__)
-
-# Add every directory inside vendor/ to sys.path.
-for file in os.listdir('vendor'):
-    path = os.path.join(BASE_DIR, 'vendor', file)
-    if os.path.isdir(path) and path not in sys.path:
-        sys.path.append(path)
-
-import click  # isort:skip
-from jsonrpc import dispatcher, JSONRPCResponseManager  # isort:skip
-
-# FIXME: This must be an positional argument.  See #28.
-import anaconda_jedi   # noqa isort:skip
-import anaconda_eldoc  # noqa isort:skip
+    def import_module(name):
+        __import__(name)
+        return sys.modules[name]
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
@@ -50,21 +55,33 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
     @staticmethod
     def process_request(request):
+        from jsonrpc import dispatcher, JSONRPCResponseManager
         response = JSONRPCResponseManager.handle(request, dispatcher)
         status = 500 if response.error else 200
         return status, response.json
 
 
+import click  # noqa isort:skip
+
+
 @click.command()
 @click.option('--bind', default='', help='Interface address to bind.')
 @click.option('--port', type=int, default=8000, help='Server port.')
+@click.option('paths', '--path', multiple=True, type=click.Path())
+@click.option('plugins', '--plugin', multiple=True)
 @click.option('--debug', default=False, is_flag=True,
               help='Enable debug logging.')
-def main(bind, port, debug):
+def main(bind, port, paths, plugins, debug):
     """Runs anaconda server."""
+    for path in paths:
+        add_package_path(path)
+
+    for plugin in plugins:
+        import_module('anaconda_' + plugin)
+
     if debug:
-        logging.basicConfig(level=logging.DEBUG,
-                            filename=os.path.join(LOG_DIR, 'development.log'))
+        logfile = os.path.join(BASE_DIR, 'log', 'development.log')
+        logging.basicConfig(level=logging.DEBUG, filename=logfile)
 
     logger.info('Starting anaconda_mode server...')
     server = HTTPServer((bind, port), HTTPRequestHandler)
