3267bae708266fd00b794148cbe2b11ed18ecb31
Fabio Corneti <info@corneti.com>
2014-08-31 23:00:38 +0200

    If the anaconda server port can't be read within 5 seconds kill the process.

--- anaconda-mode.el
+++ anaconda-mode.el
@@ -96,11 +96,13 @@ Return nil if it run under proper environment."
            (anaconda-mode-python)
            "anaconda_mode.py"))
     (set-process-filter anaconda-mode-process 'anaconda-mode-process-filter)
-    (accept-process-output anaconda-mode-process)
+    (accept-process-output anaconda-mode-process 5)
     (set-process-filter anaconda-mode-process nil)
     (set-process-query-on-exit-flag anaconda-mode-process nil)
     (unless anaconda-mode-port
-      (error "Unable to run anaconda_mode.py"))))
+      (progn
+        (anaconda-mode-stop)
+        (error "Unable to run anaconda_mode.py")))))
 
 (defun anaconda-mode-process-filter (process output)
   "Set `anaconda-mode-port' from PROCESS OUTPUT."
