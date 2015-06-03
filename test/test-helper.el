6df4d71ed29ac95eafd46977c281eda814701748
Artem Malyshev <proofit404@gmail.com>
2015-06-03 15:54:23 +0300

    Correct undercover.el usage.

--- test/test-helper.el
+++ test/test-helper.el
@@ -10,7 +10,7 @@
   (cask-initialize source-directory)
   (add-to-list 'load-path source-directory))
 
-(undercover "anaconda-mode.el" (:report-file "emacs-coveralls.json" :send-report nil))
+(undercover "anaconda-mode.el" (:report-file "emacs-coveralls.json") (:send-report nil))
 
 (provide 'test-helper)
 
