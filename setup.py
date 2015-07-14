841a5b819692bac247e8789bad16d09834d9be9a
Artem Malyshev <proofit404@gmail.com>
2015-07-14 19:45:58 +0300

    Exclude test directory from sdist.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 readme = open('README.rst').read()
 
 setup(name='anaconda_mode',
-      version='0.1.0',
+      version='0.1.1',
       url='https://github.com/proofit404/anaconda-mode',
       description='Code navigation, documentation lookup and '
       'completion for Python.',
