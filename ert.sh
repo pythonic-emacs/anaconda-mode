a20881c33fd6457d03ad85c416050b61ba89616b
Artem Malyshev <proofit404@gmail.com>
2015-09-08 15:19:57 +0300

    Run ert test on IPython.

--- ert.sh
+++ ert.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -e
 
-for emacs in emacs-24.3 emacs-24.4 emacs-24.5
-do
-    evm use $emacs
-    emacs --version
-    cask exec ert-runner $@
-done
+function run_ert() {
+    for emacs in emacs-24.3 emacs-24.4 emacs-24.5
+    do
+        evm use $emacs
+        emacs --version
+        echo "Python version:"
+        pyenv version
+        cask exec ert-runner $@
+    done
+}
+
+run_ert
+
+export PYENV_VERSION=ipython
+run_ert
