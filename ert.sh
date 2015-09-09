85fb53d1989c348ecae188c207983f647f7b784f
Artem Malyshev <proofit404@gmail.com>
2015-09-09 15:11:27 +0300

    Clean up installation before test run.

--- ert.sh
+++ ert.sh
@@ -3,6 +3,8 @@
 function run_ert() {
     for emacs in emacs-24.3 emacs-24.4 emacs-24.5
     do
+        echo "On boot cleanup."
+        rm -rf $HOME/.emacs.d/anaconda-mode/
         evm use $emacs
         emacs --version
         echo "Python version:"
@@ -11,6 +13,7 @@ function run_ert() {
     done
 }
 
+unset PYENV_VERSION
 run_ert
 
 export PYENV_VERSION=ipython
