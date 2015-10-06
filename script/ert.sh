bcc63eea3ce630db9fde99f0661531eeb6607498
Artem Malyshev <proofit404@gmail.com>
2015-10-06 14:36:43 +0300

    Improve pre test cleanup.
    Remove remote installation files.

--- script/ert.sh
+++ script/ert.sh
@@ -1,15 +1,25 @@
 #!/bin/bash -e
 
+function cleanup() {
+    echo -ne '\e[01;34m'"On boot cleanup... "'\e[0m'
+    rm -rf $HOME/.emacs.d/anaconda-mode/
+    ssh test@localhost rm -rf $HOME/.emacs.d/anaconda-mode/
+    echo done
+}
+
+function info() {
+    echo -ne '\e[01;34m'"Emacs version: "'\e[0m'
+    echo $emacs
+    echo -ne '\e[01;34m'"Python version: "'\e[0m'
+    echo -n "$(python -V) ($(pyenv which python))"
+    echo
+}
+
 function run_ert() {
     for emacs in emacs-24.3 emacs-24.4 emacs-24.5
     do
-        echo -ne '\e[01;34m'"On boot cleanup... "'\e[0m'
-        rm -rf $HOME/.emacs.d/anaconda-mode/
-        echo done
-        echo -ne '\e[01;34m'"Emacs version: "'\e[0m'
-        echo $emacs
-        echo -ne '\e[01;34m'"Python version: "'\e[0m'
-        python -V
+        cleanup
+        info
         EMACS=$emacs cask exec ert-runner $@
     done
 }
