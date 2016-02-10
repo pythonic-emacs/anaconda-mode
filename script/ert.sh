e2bd864e3927903c432ab2ddf6cfcf69c333f591
Artem Malyshev <proofit404@gmail.com>
2016-02-10 14:13:38 +0300

    Run all tests together.

--- script/ert.sh
+++ script/ert.sh
@@ -10,29 +10,16 @@ function info_interpreter() {
     echo $interpreter
 }
 
-function run_unit() {
-    echo -e '\e[01;34m'"Run unit tests."'\e[0m\n'
-    for emacs in emacs-24.3 emacs-24.4 emacs-24.5 emacs-25.0
-    do
-        info_emacs
-        EMACS=$emacs cask exec ert-runner test/unit-test.el
-    done
-}
-
-function run_integration() {
-    echo -e '\e[01;34m'"Run integration tests."'\e[0m\n'
+function run() {
     for emacs in emacs-24.3 emacs-24.4 emacs-24.5 emacs-25.0
     do
         for interpreter in test/interpreters/*.el
         do
             info_emacs
             info_interpreter
-            EMACS=$emacs cask exec ert-runner test/integration-test.el -l $interpreter
+            EMACS=$emacs cask exec ert-runner -l $interpreter
         done
     done
 }
 
-run_unit
-echo
-
-run_integration
+run
