f69845d5a7c95a0add7ba28a9f9d755789ec1358
Artem Malyshev <proofit404@gmail.com>
2016-02-11 21:59:41 +0300

    Add missing libxpm build dependency.

--- script/install_emacs.sh
+++ script/install_emacs.sh
@@ -13,8 +13,9 @@ sudo apt-get update
 
 # Install Emacs build dependencies.
 
-sudo apt-get install -y autoconf automake build-essential git \
-     libgif-dev libncurses5-dev libpng-dev libtiff4-dev texinfo
+sudo apt-get install -y autoconf automake build-essential git      \
+     libgif-dev libncurses5-dev libpng-dev libtiff4-dev libxpm-dev \
+     texinfo
 
 # Build Emacs.
 
