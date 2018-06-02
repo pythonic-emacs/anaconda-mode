3c63f68c5808a6323fc30809c89a3cf2dd6a0707
Artem Malyshev <proofit404@gmail.com>
2018-06-02 21:26:51 +0300

    Work on recent Emacs only.

--- anaconda-mode.el
+++ anaconda-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Artem Malyshev <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/anaconda-mode
 ;; Version: 0.1.12
-;; Package-Requires: ((emacs "24") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2"))
+;; Package-Requires: ((emacs "26.1") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
